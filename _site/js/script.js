5485bbf2ab9bd3483ce32406718eeed9c31c331c
Zhenya Zozulya <zeawork@itsua.org>
2023-10-06 12:14:23 +0300

    menuActive

--- _site/js/script.js
+++ _site/js/script.js
@@ -1,46 +1,32 @@
-// Вибираємо елемент body
+
 let body = document.querySelector('body');
 let menuBtn = document.querySelector('.menu-btn');
+let overlay = document.createElement('div');
+    
+overlay.classList.add('site-overlay');
+body.appendChild(overlay);
 
 // Додаємо обробник події 'click' для елементів з класом '.menu-btn' в 'body'
 body.addEventListener('click', function(event){
-    
     // Перевіряємо, чи був клік на елементі з класом '.menu-btn'
-    if (event.target.classList.contains('menu-btn')) {
-        // Змінюємо клас 'btnActive' для поточного елемента
-        menuBtn.classList.toggle('btnActive');
+    if (event.target.classList.contains('menu-btn') || event.target.closest('.menu-btn')) {
         
+        body.classList.toggle('menuActive');
         // Перевіряємо, чи тепер елемент має клас 'btnActive'
-        if (event.target.classList.contains('btnActive')) {
-            // Додаємо клас 'menuActive' до 'body'
-            body.classList.add('menuActive');
+        if (body.classList.contains('menuActive')) {
             
-            // Створюємо новий елемент div з класом '.site-overlay'
-            var overlay = document.createElement('div');
-            overlay.classList.add('site-overlay');
-            
-            // Додаємо новий елемент до 'body'
-            body.appendChild(overlay);
         } else {
             // Видаляємо клас 'menuActive' з 'body'
             body.classList.remove('menuActive');
-            
-            // Знаходимо всі елементи з класом '.site-overlay' і видаляємо їх
-            document.querySelector('.site-overlay').remove();
-            
-            // Знаходимо всі елементи з класом '.menu-btn' і видаляємо клас 'btnActive'
-            event.target.classList.remove('btnActive');
         }
     }
     // Перевіряємо, чи був клік на елементі з класом '.site-overlay'
     if (event.target.classList.contains('site-overlay')) {
         // Видаляємо клас 'menuActive' з 'body'
         body.classList.remove('menuActive');
-        
-        // Знаходимо всі елементи з класом '.site-overlay' і видаляємо їх
-        document.querySelector('.site-overlay').remove();
-            
-        // Знаходимо всі елементи з класом '.menu-btn' і видаляємо клас 'btnActive'
-        event.target.classList.remove('btnActive');
     }
+});
+
+window.addEventListener('resize', function() {
+    document.body.classList.remove('menuActive');
 });
